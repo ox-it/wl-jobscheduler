56f9d434dbe19c910d81c132d7a29f55d6e6a7ac
Matthew Buckett <matthew.buckett@it.ox.ac.uk>
2014-07-25 17:59:42 +0100

    Just use quartz as it was intended.

--- scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/ScheduledInvocationManagerImpl.java
+++ scheduler-component-shared/src/java/org/sakaiproject/component/app/scheduler/ScheduledInvocationManagerImpl.java
@@ -1,33 +1,25 @@
 package org.sakaiproject.component.app.scheduler;
 
-import java.util.Date;
-import java.util.List;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.SchedulerException;
-import org.quartz.SimpleTrigger;
-import org.quartz.Trigger;
+import org.quartz.*;
 import org.sakaiproject.api.app.scheduler.DelayedInvocation;
 import org.sakaiproject.api.app.scheduler.ScheduledInvocationManager;
 import org.sakaiproject.api.app.scheduler.SchedulerManager;
-import org.sakaiproject.component.api.ServerConfigurationService;
-import org.sakaiproject.component.app.scheduler.jobs.ScheduledInvocationRunner;
-import org.sakaiproject.db.api.SqlService;
+import org.sakaiproject.component.app.scheduler.jobs.ScheduledInvocationJob;
 import org.sakaiproject.id.api.IdManager;
 import org.sakaiproject.time.api.Time;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 public class ScheduledInvocationManagerImpl implements ScheduledInvocationManager {
 
 	private static final Log LOG = LogFactory.getLog(ScheduledInvocationManagerImpl.class);
+	public static final String GROUP_NAME = "org.sakaiproject.component.app.scheduler.jobs.ScheduledInvocationJob";
 
-	private static final String SCHEDULED_INVOCATION_RUNNER_DEFAULT_INTERVAL_PROPERTY = "jobscheduler.invocation.interval";
 
-	private static final int SCHEDULED_INVOCATION_RUNNER_DEFAULT_INTERVAL = 120; //default: time in seconds, run every 2 mins
-
-	
 	/** Dependency: IdManager */
 	protected IdManager m_idManager = null;
 
@@ -35,110 +27,53 @@ public class ScheduledInvocationManagerImpl implements ScheduledInvocationManage
 		m_idManager = service;
 	}
 
-	/** Dependency: SqlService */
-	protected SqlService m_sqlService = null;
-
-	public void setSqlService(SqlService service) {
-		m_sqlService = service;
-	}
-	
-
 	/** Dependency: SchedulerManager */
 	protected SchedulerManager m_schedulerManager = null;
 
 	public void setSchedulerManager(SchedulerManager service) {
 		m_schedulerManager = service;
 	}
-	
-	
-	/** Dependency: ServerConfigurationService */
-	protected ServerConfigurationService m_serverConfigurationService = null;
 
-	public void setServerConfigurationService(ServerConfigurationService service) {
-		m_serverConfigurationService = service;
-	}
-	
 
-	/** Configuration: Should we autocreate our database tables */
-	protected boolean autoDdl;
 
-	public void setAutoDdl(boolean autoDdl) {
-		this.autoDdl = autoDdl;
-	}
-	
-	
 
 	public void init() {
 		LOG.info("init()");
-		if (autoDdl) {
-			m_sqlService.ddl(getClass().getClassLoader(), "delayed");
-			// Create the additional indexes as hibernate currently fails todo this.
-			// Once hibernate is updated to 3.6 it should be able to manage in it's own.
-			m_sqlService.ddl(getClass().getClassLoader(), "indexes");
-		}
-	      try {
-	          registerScheduledInvocationRunner();
-	       } catch (SchedulerException e) {
-	          LOG.error("failed to schedule ScheduledInvocationRunner job", e);
-	       }
-	    }
+    }
 
    public void destroy() {
       LOG.info("destroy()");
    }
 
-   protected void registerScheduledInvocationRunner() throws SchedulerException {
-	   
-	   //trigger will not start immediately, wait until interval has passed before 1st run
-	   long startTime = System.currentTimeMillis() + getScheduledInvocationRunnerInterval();
-	   
-       JobDetail detail = new JobDetail("org.sakaiproject.component.app.scheduler.ScheduledInvocationManagerImpl.runner",
-          "org.sakaiproject.component.app.scheduler.ScheduledInvocationManagerImpl", ScheduledInvocationRunner.class);
-
-       Trigger trigger = new SimpleTrigger("org.sakaiproject.component.app.scheduler.ScheduledInvocationManagerImpl.runner",
-          "org.sakaiproject.component.app.scheduler.ScheduledInvocationManagerImpl", new Date(startTime), null, SimpleTrigger.REPEAT_INDEFINITELY,
-          getScheduledInvocationRunnerInterval());
-
-       Scheduler scheduler = m_schedulerManager.getScheduler();
-       // This removes the jobs if there are no trigger left on it.
-       scheduler.unscheduleJob(trigger.getName(), trigger.getGroup());
-       scheduler.scheduleJob(detail, trigger);
-    }
-   
-   protected int getScheduledInvocationRunnerInterval() {
-	   
-	   // convert seconds to millis
-	   return 1000 * m_serverConfigurationService.getInt(SCHEDULED_INVOCATION_RUNNER_DEFAULT_INTERVAL_PROPERTY, SCHEDULED_INVOCATION_RUNNER_DEFAULT_INTERVAL);
-	   
-   }
-   
 	/* (non-Javadoc)
 	 * @see org.sakaiproject.api.app.scheduler.ScheduledInvocationManager#createDelayedInvocation(org.sakaiproject.time.api.Time, java.lang.String, java.lang.String)
 	 */
 	public String createDelayedInvocation(Time time, String componentId, String opaqueContext) {
+		try {
+			String uuid = m_idManager.createUuid();
+			Scheduler scheduler = m_schedulerManager.getScheduler();
+			JobDetail detail =
+					scheduler.getJobDetail(componentId, GROUP_NAME);
+			if (detail == null) {
+				detail = new JobDetail(componentId, GROUP_NAME, ScheduledInvocationJob.class);
+				detail.setDurability(true);
+				scheduler.addJob(detail, false);
+			}
 
-		String uuid = m_idManager.createUuid();
+			// Non-repeating trigger.
+			Trigger trigger = new SimpleTrigger(uuid, GROUP_NAME, new Date(time.getTime()));
+			trigger.getJobDataMap().put("contextId", opaqueContext);
+			trigger.setJobName(componentId);
+			trigger.setJobGroup(GROUP_NAME);
 
-		LOG.debug("Creating new Delayed Invocation: " + uuid);
-		String sql = "INSERT INTO SCHEDULER_DELAYED_INVOCATION VALUES(?,?,?,?)";
-
-		Object[] fields = new Object[4];
-
-		fields[0] = uuid;
-		fields[1] = time;
-		fields[2] = componentId;
-		fields[3] = opaqueContext;
-
-		LOG.debug("SQL: " + sql);
-		if (m_sqlService.dbWrite(sql, fields)) {
+			scheduler.scheduleJob(trigger);
 			LOG.info("Created new Delayed Invocation: uuid=" + uuid);
 			return uuid;
-		} else {
-			LOG.error("Failed to create new Delayed Invocation: componentId=" + componentId + 
-					", opaqueContext=" + opaqueContext);
+		} catch (SchedulerException se) {
+			LOG.error("Failed to create new Delayed Invocation: componentId=" + componentId +
+					", opaqueContext=" + opaqueContext, se);
 			return null;
 		}
-
 	}
 
 	/* (non-Javadoc)
@@ -147,13 +82,11 @@ public class ScheduledInvocationManagerImpl implements ScheduledInvocationManage
 	public void deleteDelayedInvocation(String uuid) {
 
 		LOG.debug("Removing Delayed Invocation: " + uuid);
-		String sql = "DELETE FROM SCHEDULER_DELAYED_INVOCATION WHERE INVOCATION_ID = ?";
-
-		Object[] fields = new Object[1];
-		fields[0] = uuid;
-
-		LOG.debug("SQL: " + sql);
-		m_sqlService.dbWrite(sql, fields);
+		try {
+			m_schedulerManager.getScheduler().unscheduleJob(uuid, GROUP_NAME);
+		} catch (SchedulerException e) {
+			LOG.error("Failed to remove Delayed Invocation: uuid="+ uuid);
+		}
 
 	}
 
@@ -163,37 +96,31 @@ public class ScheduledInvocationManagerImpl implements ScheduledInvocationManage
 	public void deleteDelayedInvocation(String componentId, String opaqueContext) {
 		LOG.debug("componentId=" + componentId + ", opaqueContext=" + opaqueContext);
 
-		//String sql = "DELETE FROM SCHEDULER_DELAYED_INVOCATION WHERE COMPONENT = ? AND CONTEXT = ?";
-		String sql = "DELETE FROM SCHEDULER_DELAYED_INVOCATION";
-
-		Object[] fields = new Object[0];
-		if (componentId.length() > 0 && opaqueContext.length() > 0) {
-			// both non-blank
-			sql += " WHERE COMPONENT = ? AND CONTEXT = ?";
-			fields = new Object[2];
-			fields[0] = componentId;
-			fields[1] = opaqueContext;
-		} else if (componentId.length() > 0) {
-			// context blank
-			sql += " WHERE COMPONENT = ?";
-			fields = new Object[1];
-			fields[0] = componentId;
-		} else if (opaqueContext.length() > 0) {
-			// component blank
-			sql += " WHERE CONTEXT = ?";
-			fields = new Object[1];
-			fields[0] = opaqueContext;
-		} else {
-			// both blank
+		try {
+			Scheduler scheduler = m_schedulerManager.getScheduler();
+			String[] jobNames = scheduler.getJobNames(GROUP_NAME);
+			for (String jobName : jobNames) {
+				if (componentId.length() > 0 && !(jobName.equals(componentId))) {
+					// If we're filtering by component Id and it doesn't match skip.
+					continue;
+				}
+				JobDetail detail = scheduler.getJobDetail(jobName, GROUP_NAME);
+				if (detail != null) {
+					Trigger[] triggers = scheduler.getTriggersOfJob(jobName, GROUP_NAME);
+					for (Trigger trigger: triggers) {
+						String contextId = trigger.getJobDataMap().getString("contextId");
+						if (opaqueContext.length() > 0 && !(opaqueContext.equals(contextId))) {
+							// If we're filtering by opaqueContent and it doesn't match skip.
+							continue;
+						}
+						// Unscehdule the trigger.
+						scheduler.unscheduleJob(trigger.getName(), trigger.getGroup());
+					}
+				}
+			}
+		} catch (SchedulerException se) {
+			LOG.error("Failure while attempting to remove invocations matching: componentId=" + componentId + ", opaqueContext=" + opaqueContext, se);
 		}
-
-		LOG.debug("SQL: " + sql);
-		if ( m_sqlService.dbWrite(sql, fields) ) {
-			LOG.info("Removed all scheduled invocations matching: componentId=" + componentId + ", opaqueContext=" + opaqueContext);
-		} else {
-			LOG.error("Failure while attempting to remove invocations matching: componentId=" + componentId + ", opaqueContext=" + opaqueContext);
-		}
-
 	}
 
 	/* (non-Javadoc)
@@ -201,33 +128,32 @@ public class ScheduledInvocationManagerImpl implements ScheduledInvocationManage
 	 */
 	public DelayedInvocation[] findDelayedInvocations(String componentId, String opaqueContext) {
 		LOG.debug("componentId=" + componentId + ", opaqueContext=" + opaqueContext);
-
-		//String sql = "SELECT * FROM SCHEDULER_DELAYED_INVOCATION WHERE COMPONENT = ? AND CONTEXT = ?";
-		String sql = "SELECT * FROM SCHEDULER_DELAYED_INVOCATION";
-
-		Object[] fields = new Object[0];
-		if (componentId.length() > 0 && opaqueContext.length() > 0) {
-			// both non-blank
-			sql += " WHERE COMPONENT = ? AND CONTEXT = ?";
-			fields = new Object[2];
-			fields[0] = componentId;
-			fields[1] = opaqueContext;
-		} else if (componentId.length() > 0) {
-			// context blank
-			sql += " WHERE COMPONENT = ?";
-			fields = new Object[1];
-			fields[0] = componentId;
-		} else if (opaqueContext.length() > 0) {
-			// component blank
-			sql += " WHERE CONTEXT = ?";
-			fields = new Object[1];
-			fields[0] = opaqueContext;
-		} else {
-			// both blank
+		List<DelayedInvocation> invocations = new ArrayList<DelayedInvocation>();
+		try {
+			Scheduler scheduler = m_schedulerManager.getScheduler();
+			String[] jobNames = scheduler.getJobNames(GROUP_NAME);
+			for (String jobName : jobNames) {
+				if (componentId.length() > 0 && !(jobName.equals(componentId))) {
+					// If we're filtering by component Id and it doesn't match skip.
+					continue;
+				}
+				JobDetail detail = scheduler.getJobDetail(jobName, GROUP_NAME);
+				if (detail != null) {
+					Trigger[] triggers = scheduler.getTriggersOfJob(jobName, GROUP_NAME);
+					for (Trigger trigger: triggers) {
+						String contextId = trigger.getJobDataMap().getString("contextId");
+						if (opaqueContext.length() > 0 && !(opaqueContext.equals(contextId))) {
+							// If we're filtering by opaqueContent and it doesn't match skip.
+							continue;
+						}
+						// Add this one to the list.
+						invocations.add(new DelayedInvocation(trigger.getName(), trigger.getNextFireTime(), jobName, contextId));
+					}
+				}
+			}
+		} catch (SchedulerException se) {
+			LOG.error("Failure while attempting to remove invocations matching: componentId=" + componentId + ", opaqueContext=" + opaqueContext, se);
 		}
-
-		List invocations = m_sqlService.dbRead(sql, fields, new DelayedInvocationReader());
-		return (DelayedInvocation[]) invocations.toArray( new DelayedInvocation[] {} );
+		return invocations.toArray(new DelayedInvocation[]{});
 	}
-
 }
